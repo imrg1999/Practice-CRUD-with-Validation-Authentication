3b16b825075e320461203edd1fe11898f0a7ca19
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-08-15 11:52:25 +0530

    Model created and Schema defined

---
+++ Model/userModel.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    contact: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    timestamps: true 
+});
+
+export const userModel = model("userValidation", userSchema);

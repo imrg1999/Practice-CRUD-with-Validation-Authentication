2104304222327b52c651872b716d0caf3e5dae11
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-08-15 12:48:50 +0530

    Route mounted on index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { connectDB } from './Config/connectDB.js';
+import userRoutes from './Routes/userRoutes.js'
 
 const port = 3000;
 const app = express();
+
+//Route establishment
+app.use('/api',userRoutes);
 
 //DB connection
 connectDB();
